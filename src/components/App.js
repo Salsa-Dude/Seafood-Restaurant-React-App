d28d79bd66abc2170a81906b978dd8f1b05c9932
Joseph Arias <arias.joseph.a@gmail.com>
2018-07-25 16:25:32 -0400

    Load data into state

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React, { Fragment } from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
 
@@ -22,6 +23,12 @@ class App extends React.Component {
     })
   }
 
+  loadSampleFishes = () => {
+    this.setState({
+      fishes: sampleFishes
+    });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -29,7 +36,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
       </div>
     )
   }
